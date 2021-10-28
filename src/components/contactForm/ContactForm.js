9051748a05cab5f0b6e8a3e66fcab4956a98b6c5
Emhemmed Abdussalam <39777333+E-Abdussalam@users.noreply.github.com>
2021-10-28 19:47:03 +0100

    first commit

---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const ContactForm = ({
+  name,
+  setName,
+  phone,
+  setPhone,
+  email,
+  setEmail,
+  handleSubmit
+}) => {
+  return (
+   <form onSubmit={handleSubmit}>
+     <input
+     type='text'
+     value={name}
+     onChange={({target}) => {setName(target.value)}}
+     required
+     placeholder='Conatct Name'
+     />
+     <input
+     type='tel'
+     value={phone}
+     pattern="\D*([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D*"
+     onChange={({target}) => {setPhone(target.value)}}
+     required
+     placeholder= '###-###-####'
+     />
+     <input
+     type='email'
+     value={email}
+     onChange={({target}) =>{setEmail(target.value)}}
+     placeholder='email@example.com'
+     />
+     <input
+     type= 'submit'
+     />
+      </form>
+  );
+};
